da99a0fe0cfbde4d4aae0bf20e1e6fdde87ab8d1
Martin Richter <martin.richter@futurice.com>
2014-11-23 15:54:49 +0100

    Display list of available players in SelectPlayersViewController

--- ObjectiveCalcio/View/SelectPlayersViewController.m
+++ ObjectiveCalcio/View/SelectPlayersViewController.m
@@ -7,12 +7,50 @@
 //
 
 #import "SelectPlayersViewController.h"
-#import "EditMatchViewModel.h"
+#import "SelectPlayersViewModel.h"
+#import "UIViewController+Active.h"
+#import <libextobjc/EXTScope.h>
+#import <ReactiveCocoa/ReactiveCocoa.h>
+
+static NSString * const PlayerCellIdentifier = @"PlayerCell";
 
 @interface SelectPlayersViewController ()
 
 @end
 
 @implementation SelectPlayersViewController
+
+#pragma mark - View Lifecycle
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    @weakify(self);
+
+    [self.viewModel.updatedContentSignal subscribeNext:^(id _) {
+        @strongify(self);
+        [self.tableView reloadData];
+    }];
+
+    RAC(self.viewModel, active) = self.activeSignal;
+}
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return [self.viewModel numberOfSections];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [self.viewModel numberOfItemsInSection:section];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:PlayerCellIdentifier forIndexPath:indexPath];
+
+    cell.textLabel.text = [self.viewModel playerNameAtRow:indexPath.row inSection:indexPath.section];
+
+    return cell;
+}
 
 @end
