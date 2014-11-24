fae7904541ce1a331e825b21f66d306a35212bab
Martin Richter <martin.richter@futurice.com>
2014-11-24 01:12:40 +0100

    Improve UX when there are no Matches yet

--- ObjectiveCalcio/View/MatchesViewController.m
+++ ObjectiveCalcio/View/MatchesViewController.m
@@ -12,6 +12,7 @@
 #import "MatchesViewModel.h"
 #import "EditMatchViewModel.h"
 #import "UIViewController+Active.h"
+#import <DZNEmptyDataSet/UIScrollView+EmptyDataSet.h>
 #import <JGProgressHUD/JGProgressHUD.h>
 #import <JGProgressHUDFadeZoomAnimation.h>
 #import <libextobjc/EXTScope.h>
@@ -19,7 +20,7 @@
 static NSString * const MatchCellIdentifier = @"MatchCell";
 static NSString * const EditMatchSegueIdentifier = @"EditMatch";
 
-@interface MatchesViewController ()
+@interface MatchesViewController () <DZNEmptyDataSetSource>
 
 @end
 
@@ -29,6 +30,9 @@ static NSString * const EditMatchSegueIdentifier = @"EditMatch";
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+
+    self.tableView.emptyDataSetSource = self;
+    self.tableView.tableFooterView = [UIView new];
 
     @weakify(self);
 
@@ -50,6 +54,26 @@ static NSString * const EditMatchSegueIdentifier = @"EditMatch";
     }];
 
     RAC(self.viewModel, active) = self.activeSignal;
+}
+
+#pragma mark - DZNEmptyDataSetSource
+
+- (NSAttributedString *)titleForEmptyDataSet:(UIScrollView *)scrollView {
+    NSString *text = @"No matches yet!";
+    NSDictionary *attributes = @{
+        NSFontAttributeName: [UIFont fontWithName:@"OpenSans-Semibold" size:30],
+        NSForegroundColorAttributeName: [UIColor darkGrayColor]
+    };
+    return [[NSAttributedString alloc] initWithString:text attributes:attributes];
+}
+
+- (NSAttributedString *)descriptionForEmptyDataSet:(UIScrollView *)scrollView {
+    NSString *text = @"Tap the Add button at the top-right corner to get started.";
+    NSDictionary *attributes = @{
+        NSFontAttributeName: [UIFont fontWithName:@"OpenSans" size:20],
+        NSForegroundColorAttributeName: [UIColor lightGrayColor]
+    };
+    return [[NSAttributedString alloc] initWithString:text attributes:attributes];
 }
 
 #pragma mark - UITableViewDataSource
